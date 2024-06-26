017ef2e2cf981496606b7f7486ad68dd2f495ccb
Mateocheveri <cros746@gmail.com>
2024-06-26 13:40:12 -0500

    explicacion del final

--- trabajo final/app.js
+++ trabajo final/app.js
@@ -65,3 +65,63 @@ btn[8].addEventListener("click", cambio9);
 btn[9].addEventListener("click", cambio10);
 btn[10].addEventListener("click", cambio11);
 btn[11].addEventListener("click", cambio12);
+
+
+
+
+/* sustentacion */
+
+/* 
+la primera interaccion es en el inicio, son las targetas seleccionables con efecto hover se mostrara un texto relacionado relacionado a esa tarjeta,
+con estos pasos se puede hacer:
+
+1:variable.
+const box = document.querySelector(".box")   (se llaman las clases que tienen las targetas tanto el texto como las imagenes)
+const text = document.querySelector(".text")
+const icon = document.querySelector(".img")
+
+2:funciones
+
+function boxel(){
+pular la lista de clases.)
+ textt.classlist. boxl.classlist.toggle('boxselected') (el classlist es es una propiedad de solo lectura que devuelve una colección DOMTokenList activa de los atributos de clase del elemento. Esto luego se puede usar para manitoggle(".box")
+ icon.clssList.toggle
+}
+
+3: el evento
+box.addEventListener("click", box_selected1) (al darle clic a la card resaltara el texto oculto dentro de las tarjetas)
+*/
+
+/* los botones tiene un efecto que muestra el texto, esto es ya que al darle clck a el voton "ver mas"  
+resaltara una ventana con la informacion deseada 
+*/
+/* 
+con estos pasos se podra hacer:
+llamamos las variables que seria la ventana con el texto emergente y el bootn
+
+const btn = document.getElement('.btn');
+const texto = document.getelement('texin');
+let e = 0;
+
+//2.Funciones
+function botontex(e){
+  "en la funcion tendra una animacion de salida de .5s y condicionales esto es ya que al darle a el boton se abrira y se cerrara el texto"
+  texto.classList.toggle('show')
+
+  if(text.classList.contains('show')){
+    text.style.marginLeft = '400px';
+    text.innerhtml = 'informacion '
+    pelota.style.transitionDuration = '.5s';
+    e=1;
+  }
+  else{
+    pelota.style.marginLeft = '0px';
+    btn.innerhtml = 'closed'
+    pelota.style.transitionDuration = '.5s';
+    e=0;
+  }
+}
+
+3: el evento
+btn.addEventListener("click", botontext)
+ (al darle clic a el btn resaltara saldra el texto y al volverle a dar a el botno se ocultara el texto);
